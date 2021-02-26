481114cd1557094f069f7f71e608cfe16e73bd7a
Nicolas Depelteau <nicolas.depelteau@polymtl.ca>
2021-02-25 22:21:30 -0500

    ND: missing the comparaison in admis thing

--- src/exo2/admisasm.s
+++ src/exo2/admisasm.s
@@ -8,7 +8,22 @@ _ZN8Etudiant8admisasmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
-        / * Programme ici   */
+        movl 8(%ebp), %eax			# met this dans eax
         
-        leave          /* restore ebp and esp */
-        ret            /* return to the caller */
+	pushl %eax				# envoie this en parametre
+	call _ZN8Etudiant10moyenneasmEv 	# appel moyennecpp() 
+
+	movl $10, %edx	# comparaison icitte a faire marche
+	fld %edx
+	fucomp %st(1)				# if (moyenne >=10)
+	jb REFUS
+ADMIS:
+        movl $1, %eax				# return 1
+        jmp FIN
+
+REFUS:
+        movl $0, %eax				# return 0
+FIN:    
+	movl %edx, %eax
+        leave					/* restore ebp and esp */
+        ret					/* return to the caller */
