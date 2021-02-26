6228ff7ac722465b041c26faf585cfd9cefa2dc5
Depdx <71102008+Depdx@users.noreply.github.com>
2021-02-25 19:13:10 -0500

    exo 2
    Vous devez implémenter en assembleur deux fonctions écrites en C++.
    • Etudiant::moyenneasm()
    • Etudiant::admisasm()
    Les méthodes correspondantes en C++ sont :
    • Etudiant::moyennecpp()
    • Etudiant::admiscpp()

--- src/exo2/admisasm.s
+++ src/exo2/admisasm.s
@@ -8,7 +8,7 @@ _ZN8Etudiant8admisasmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
-        / * Programme ici */
+        / * Programme ici   */
         
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
