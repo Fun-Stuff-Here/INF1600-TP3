ee3e203ea3fa62be0d41b06d6c9c53643898cff4
Nicolas Depelteau <nicolas.depelteau@polymtl.ca>
2021-02-28 17:22:39 -0500

    ND: my brain at his point is not working

--- src/exo3/admisasm1.s
+++ src/exo3/admisasm1.s
@@ -3,10 +3,32 @@
 .globl	_ZN18EtudiantEnMaitrise8admisasmEv
 
 _ZN18EtudiantEnMaitrise8admisasmEv:
-        push %ebp      /* sauver le pointeur ebp */
-        mov %esp, %ebp /* ebp recoit esp */
-        
-        / * Programme ici */
-        
-        leave          /* recuperer ebp et esp */
-        ret          
+	push %ebp      /* save old base pointer */
+	mov %esp, %ebp /* set ebp to current esp */
+	pushl %ebx
+
+	movl 8(%ebp), %ebx			# met this dans ebx
+
+	pushl %ebx				# envoie this en parametre
+	call _ZN8Etudiant10moyenneasmEv 	# appel moyennecpp() 
+
+	pushl $10
+	fild (%esp)
+	fcomi %st(1)				# if (moyenne >=10)
+	ja REFUS
+
+	# movl (%ebx), %ecx 
+	# cmp $10, %ecx	 			# if ((note_memoire >=10))
+	# jb REFUS
+
+ADMIS:
+	movl $1, %eax				# return 1
+	jmp FIN
+
+REFUS:
+	movl $0, %eax				# return 0
+FIN:
+	popl %ebx
+	leave					/* restore ebp and esp */
+	ret					/* return to the caller */
+
--- src/exo3/moyenneasm.s
+++ src/exo3/moyenneasm.s
@@ -5,10 +5,24 @@
 .globl	_ZN8Etudiant10moyenneasmEv
 
 _ZN8Etudiant10moyenneasmEv :
-        push %ebp      /* sauver le pointeur ebp */
-        mov %esp, %ebp /* ebp recoit esp */
-        
-        / * Programme ici */
+		pushl %ebp					# sauver le pointeur ebp 
+		movl %esp, %ebp					# ebp recoit esp 
+		pushl %ebx
+		
+		movl 8(%ebp), %ebx				# met this dans ebx
+		movl $10, %ecx					# i= 10, nombre d'iteration
+		fldz						# som =0; 
 
-        leave          /* restore ebp and esp */
-        ret            /* return to the caller */
+FORLOOP:
+		fadd (%ebx,%ecx,4) 				# som += tabnotes[i]
+		loop FORLOOP
+
+		fdiv factor					# som/10
+		
+RETOUR:
+		popl %ebx
+		leave						# restore ebp and esp 
+		ret						# return to the caller 
+
+
+
