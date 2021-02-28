5c465ac8d9c959a8427ea7de66f149edb95ddd9e
Nicolas Depelteau <nicolas.depelteau@polymtl.ca>
2021-02-28 15:37:18 -0500

    ND:solution primaire fonctionnel

--- src/exo2/admisasm.s
+++ src/exo2/admisasm.s
@@ -5,25 +5,24 @@
 .globl	_ZN8Etudiant8admisasmEv
 
 _ZN8Etudiant8admisasmEv:
-        push %ebp      /* save old base pointer */
-        mov %esp, %ebp /* set ebp to current esp */
-        
-        movl 8(%ebp), %eax			# met this dans eax
-        
+	push %ebp      /* save old base pointer */
+	mov %esp, %ebp /* set ebp to current esp */
+
+	movl 8(%ebp), %eax			# met this dans eax
+
 	pushl %eax				# envoie this en parametre
 	call _ZN8Etudiant10moyenneasmEv 	# appel moyennecpp() 
 
-	movl $10, %edx	# comparaison icitte a faire marche
-	fld %edx
-	fucomp %st(1)				# if (moyenne >=10)
-	jb REFUS
+	pushl $10
+	fild (%esp)
+	fcomi %st(1)				# if (moyenne >=10)
+	ja REFUS
 ADMIS:
-        movl $1, %eax				# return 1
-        jmp FIN
+	movl $1, %eax				# return 1
+	jmp FIN
 
 REFUS:
-        movl $0, %eax				# return 0
-FIN:    
-	movl %edx, %eax
-        leave					/* restore ebp and esp */
-        ret					/* return to the caller */
+	movl $0, %eax				# return 0
+FIN:
+	leave					/* restore ebp and esp */
+	ret					/* return to the caller */
